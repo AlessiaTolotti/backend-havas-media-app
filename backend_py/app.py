1da2ca634f744fe354e3a2144d0f7259cfe263b3
Alessia Tolotti <alessia.tolotti.24@stud.itsaltoadriatico.it>
2025-06-20 16:53:21 +0200

    second commit

--- backend_py/app.py
+++ backend_py/app.py
@@ -39,4 +39,8 @@ def login():
 
 
 if __name__ == '__main__':
+<<<<<<< HEAD
     app.run(host='0.0.0.0', port=8000, debug=True)
+=======
+    app.run(host='0.0.0.0', port=8000, debug=True)
+>>>>>>> 5aec459 (second commit)
